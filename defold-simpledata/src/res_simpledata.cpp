5620a931b3663e8a8a1072ed897d89c2d97c60ae
JCash <mwesterdahl76@gmail.com>
2024-06-10 11:23:28 +0200

    Updated to defold 1.9.0

--- defold-simpledata/src/res_simpledata.cpp
+++ defold-simpledata/src/res_simpledata.cpp
@@ -1,17 +1,17 @@
 #include <memory.h>
 
 #include <dmsdk/dlib/log.h>
-#include <dmsdk/resource/resource.h>
+#include <dmsdk/resource/resource.hpp>
 
 #include "simpledata_ddf.h" // generated from the simpledata_ddf.proto
 #include "res_simpledata.h"
 
 namespace dmSimpleData
 {
-    static dmResource::Result ResourceCreate(const dmResource::ResourceCreateParams& params)
+    static dmResource::Result ResourceCreate(const dmResource::ResourceCreateParams* params)
     {
         dmGameSystemDDF::SimpleDataDesc* desc;
-        dmDDF::Result e  = dmDDF::LoadMessage(params.m_Buffer, params.m_BufferSize, &desc);
+        dmDDF::Result e  = dmDDF::LoadMessage(params->m_Buffer, params->m_BufferSize, &desc);
         if ( e != dmDDF::RESULT_OK )
         {
             return dmResource::RESULT_FORMAT_ERROR;
@@ -20,43 +20,42 @@ namespace dmSimpleData
         // when hot reloading
         SimpleDataResource* resource = new SimpleDataResource;
         resource->m_DDF = desc;
-        params.m_Resource->m_Resource = (void*) resource;
+        dmResource::SetResource(params->m_Resource, resource);
         return dmResource::RESULT_OK;
     }
 
-    static dmResource::Result ResourceDestroy(const dmResource::ResourceDestroyParams& params)
+    static dmResource::Result ResourceDestroy(const dmResource::ResourceDestroyParams* params)
     {
-        SimpleDataResource* resource = (SimpleDataResource*) params.m_Resource->m_Resource;
+        SimpleDataResource* resource = (SimpleDataResource*) dmResource::GetResource(params->m_Resource);
         dmDDF::FreeMessage(resource->m_DDF);
         delete resource;
         return dmResource::RESULT_OK;
     }
 
-    static dmResource::Result ResourceRecreate(const dmResource::ResourceRecreateParams& params)
+    static dmResource::Result ResourceRecreate(const dmResource::ResourceRecreateParams* params)
     {
         dmGameSystemDDF::SimpleDataDesc* desc;
-        dmDDF::Result e  = dmDDF::LoadMessage(params.m_Buffer, params.m_BufferSize, &desc);
+        dmDDF::Result e  = dmDDF::LoadMessage(params->m_Buffer, params->m_BufferSize, &desc);
         if ( e != dmDDF::RESULT_OK )
         {
             return dmResource::RESULT_FORMAT_ERROR;
         }
-        SimpleDataResource* resource = (SimpleDataResource*) params.m_Resource->m_Resource;
+        SimpleDataResource* resource = (SimpleDataResource*) dmResource::GetResource(params->m_Resource);
         dmDDF::FreeMessage(resource->m_DDF);
         resource->m_DDF = desc;
         return dmResource::RESULT_OK;
     }
 
-    static dmResource::Result ResourceType_Register(dmResource::ResourceTypeRegisterContext& ctx)
+    static ResourceResult ResourceType_Register(HResourceTypeContext ctx, HResourceType type)
     {
-        return dmResource::RegisterType(ctx.m_Factory,
-                                           ctx.m_Name,
-                                           0, // context
-                                           0, // preload
-                                           ResourceCreate,
-                                           0, // post create
-                                           ResourceDestroy,
-                                           ResourceRecreate);
-
+        return (ResourceResult)dmResource::SetupType(ctx,
+                                                     type,
+                                                     0, // context
+                                                     0, // preload
+                                                     ResourceCreate,
+                                                     0, // post create
+                                                     ResourceDestroy,
+                                                     ResourceRecreate);
     }
 }
 
